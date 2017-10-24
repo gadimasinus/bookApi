12c034f2d8c51416dfa40e772de90d8edd79f78f
Manish Gadiya <Manish Gadiya>
2017-10-24 16:31:12 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+var port = process.env.port || 3000;
+
+var db = mongoose.connect('mongodb://localhost/bookAPI',{useMongoClient :true});
+var book = require('./models/bookModel');
+
+
+var bookRouter = express.Router();
+
+bookRouter.route('/books').get(function(req, res){
+        var query = {};
+        if(req.query.genre)
+        {
+            query.genre = req.query.genre;
+        }
+        book.find(query,function(err,books){
+            if(err){
+                res.status(500).send(err);
+            }
+            else
+            {
+                res.json(books);
+            }
+        });
+});
+
+app.use('/api',bookRouter);
+
+
+app.get('/',function(req,res){
+    res.send('welcome to my book api...');
+});
+
+app.listen(port, function(){
+    console.log('running on port ' + port);
+});
