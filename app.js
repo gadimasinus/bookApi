4e4f3fe06de48f2dbfda344b5e82e4d54fd8121e
Manish Gadiya <Manish Gadiya>
2017-10-26 15:02:12 -0400

    cors issue fixed

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ var port = process.env.port || 3000;
 
  var swaggerDocument = require('./swagger.json');
  app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+ app.use(function(req,res,next){
+     // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+ });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var db = mongoose.connect('mongodb://localhost/bookAPI',{useMongoClient :true});
